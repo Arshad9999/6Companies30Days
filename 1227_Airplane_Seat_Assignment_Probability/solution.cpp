f36d7c740f39f3ae981da3101bea9f5d9a20bc5e
vortex-21 <anshumanlaskar1@gmail.com>
2023-01-04 16:29:44 +0530

    Added!

---
+++ 1227_Airplane_Seat_Assignment_Probability/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+       double ans=0.5;
+       if(n == 1)
+       {
+           return 1; 
+       }
+       return ans;
+    }
+};
